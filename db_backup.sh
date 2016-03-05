13eaf76350ea809d1ff115c49e301bc7cfb0a92e
raul <raul@mgcoders.uy>
2016-03-05 10:50:44 -0500

    pgpass file

--- db_backup.sh
+++ db_backup.sh
@@ -1,1 +1,6 @@
+# Install the pgpass file to create a connection to the database without
+# the need to type in a password
+export PG_S3_BACKUP_PORT='5432'
+echo "$PG_S3_BACKUP_HOST:$PG_S3_BACKUP_PORT:$PG_S3_BACKUP_DB:$PG_S3_BACKUP_USER:$PG_S3_BACKUP_PASSWORD" >> /root/.pgpass
+chmod 0600 /root/.pgpass
 /root/s3backup_pg.sh $PG_S3_BACKUP_BUCKET $PG_S3_BACKUP_USER $PG_S3_BACKUP_PASSWORD $PG_S3_BACKUP_DB $PG_S3_BACKUP_HOST
